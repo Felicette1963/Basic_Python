06b454f43e325811110e23b7ce0d6e67544f85ee
Felicette1963 <xorudlee@gmail.com>
2023-06-07 16:14:44 +0900

    Initial commit

---
+++ Basic_01.py
@@ -0,0 +1,23 @@
+
+class Basic_01:
+    def __init__(self):
+        # self.Basic_Python_01();
+        # self.Basic_Python_02();
+        self.Basic_Python_03();
+
+    # 2023-06-07 수업
+    def Basic_Python_01(self):
+        print("Hello World!!");
+        
+    def Basic_Python_02(self):
+        # Hello world!!
+        # 출력하는 기능: print(문자열)
+        print("반가워요");
+        print("반가워요");
+        
+    def Basic_Python_03(self):
+        a = 10;
+        if a == 10:
+            print("a가 10이네요")
+        else:
+            print("a가 10이 아니라고!!");
---
+++ main.py
@@ -0,0 +1,4 @@
+import Basic_01 as Basic
+
+if __name__ == '__main__':
+     b = Basic.Basic_01();
