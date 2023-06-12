981c70794eb6ae0e77c456219653dd87ba2e59dc
Felicette1963 <xorudlee@gmail.com>
2023-06-12 17:23:04 +0900

    2023년 6월 9일 오후 5시 22분 업데이트 - IF, 딕셔너리, 인덱스 슬라이싱, 시퀀스 자료형

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import Basic_01 as Basic1
 import Basic_02 as Basic2
 import Basic_03 as Basic3
+import Basic_04 as Basic4
 
 if __name__ == '__main__':
      # b1 = Basic1.Basic_01();
      # b2 = Basic2.Basic_02();
-     b3 = Basic3.Basic_03();
+     # b3 = Basic3.Basic_03();
+     b4 = Basic4.Basic_04();
