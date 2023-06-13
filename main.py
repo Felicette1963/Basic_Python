59685786e67e5a55641156a8ec06b6a04d0f5da1
Felicette1963 <xorudlee@gmail.com>
2023-06-13 17:29:43 +0900

    2023년 6월 12일 오후 5시 28분 업데이트 - 복습 / 반복문

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import Basic_01 as Basic1
 import Basic_02 as Basic2
 import Basic_03 as Basic3
 import Basic_04 as Basic4
+import Basic_05 as Basic5
 
 if __name__ == '__main__':
      # b1 = Basic1.Basic_01();
      # b2 = Basic2.Basic_02();
      # b3 = Basic3.Basic_03();
-     b4 = Basic4.Basic_04();
+     # b4 = Basic4.Basic_04();
+     b5 = Basic5.Basic_05();
